3971213e43c1e9465a4c34e8e0c5a6544e4f0dca
ProDaddy <undefended0@gmail.com>
2020-04-30 14:05:41 +0530

    backend + frontend inital setup

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+
+const control = require('./controllers')
+const db = require('./models')
+const routes = require('./routes')
+
+//cors middleware
+app.use(cors())
+
+//middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+
+// for deployment purpose
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res) =>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
+const port = process.env.PORT || 8080
+
+app.listen(port,()=>{
+    console.log(`server running @${port}`);
+})
