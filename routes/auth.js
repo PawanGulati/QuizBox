dde5c763fa1ca55377948c0b2b29023a1f69695c
ProDaddy <undefended0@gmail.com>
2020-05-05 00:39:55 +0530

    deploy 3.0 (final touch, think so)

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const {signInValidator,signUpValidator} =  require('../validation/user')
 const {runValidation} = require('../validation')
 
 
-router.post('/register',control.signUp) //, signUpValidator, runValidation
+router.post('/register',signUpValidator,runValidation,control.signUp) //, signUpValidator, runValidation
 
 router.post('/login', signInValidator, runValidation,control.signIn)
 
